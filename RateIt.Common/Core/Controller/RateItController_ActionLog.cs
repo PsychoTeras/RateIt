8f7329f7ee4e76082bbc7779c56ff6ef3b8979ac
PsychoTeras <psychoteras@mail.ru>
2014-07-11 22:58:08 +0300

    IC

--- RateIt.Common/Core/Controller/RateItController_ActionLog.cs
+++ RateIt.Common/Core/Controller/RateItController_ActionLog.cs
@@ -21,8 +21,8 @@ namespace RateIt.Common.Core.Controller
             ObjectId? @object = null, string objectInfo = null,
             string additionalInfo = null)
         {
-            ActionLog actionLog = new ActionLog(actionType, @object,
-                objectInfo, subject, subjectInfo, additionalInfo);
+            ActionLog actionLog = new ActionLog(actionType, subject,
+                subjectInfo, @object, objectInfo, additionalInfo);
             _actionLogDAL.ActionLogAdd(actionLog);
         }
 
@@ -31,16 +31,20 @@ namespace RateIt.Common.Core.Controller
             byte[] @object = null, string objectInfo = null,
             string additionalInfo = null)
         {
-            //
+            //Convert byte arrays to ObjectId
             ObjectId? objSubject = subject == null
                 ? (ObjectId?) null
                 : subject.ToObjectId();
             ObjectId? objObject = @object == null
                 ? (ObjectId?)null
                 : @object.ToObjectId();
+
+            //Create and push log record into DB
             ActionLog actionLog = new ActionLog(actionType, objSubject,
-                objectInfo, objObject, subjectInfo, additionalInfo);
+                subjectInfo, objObject, objectInfo, additionalInfo);
             _actionLogDAL.ActionLogAdd(actionLog);
+
+            //Release ObjectId
             if (objSubject.HasValue)
             {
                 objSubject.Value.Release();
@@ -56,16 +60,20 @@ namespace RateIt.Common.Core.Controller
             string @object = null, string objectInfo = null,
             string additionalInfo = null)
         {
-            //
+            //Convert strings to ObjectId
             ObjectId? objSubject = subject == null
                 ? (ObjectId?)null
                 : subject.ToObjectId();
             ObjectId? objObject = @object == null
                 ? (ObjectId?)null
                 : @object.ToObjectId();
+
+            //Create and push log record into DB
             ActionLog actionLog = new ActionLog(actionType, objSubject,
-                objectInfo, objObject, subjectInfo, additionalInfo);
+                subjectInfo, objObject, objectInfo, additionalInfo);
             _actionLogDAL.ActionLogAdd(actionLog);
+
+            //Release ObjectId
             if (objSubject.HasValue)
             {
                 objSubject.Value.Release();
